e224633c7ef403b14ffaf3888eece309a6935296
Eda AYDIN <edaaydin.ea@outlook.com>
2024-10-04 18:57:25 +0300

    Add plotting functionality and implement save/load for canvas state
    - Introduce `plotX` method in `TerminalScribe` to draw functions on the canvas.
    - Implement `sine` and `cosine` functions for plotting.
    - Develop `Canvas.toFile` and `Canvas.fromFile` methods for saving and loading canvas states as JSON.
    - Create exceptions for invalid parameters to enhance error handling.
    - Refactor `TerminalScribe` and subclasses for improved structure and functionality.

--- exercise_files/06_04_challenge.py
+++ exercise_files/06_04_challenge.py
@@ -78,6 +78,12 @@ class TerminalScribe:
                 pos = [self.pos[0] + self.direction[0], self.pos[1] + self.direction[1]]
             self.draw(pos)
 
+    def plotX(self, function):
+        for x in range(self.canvas._x):
+            pos = [x, function(x)]
+            if pos[1] and not self.canvas.hitsWall(pos):
+                self.draw(pos)
+
     def drawSquare(self, size):
         for i in range(size):
             self.right()
@@ -96,9 +102,16 @@ class TerminalScribe:
         time.sleep(self.framerate)
 
 
+def sine(x):
+    return 5*math.sin(x/4) + 10
+
+def cosine(x):
+    return 5*math.cos(x/4) + 10
+
+
 canvas = Canvas(30, 30)
 scribe = TerminalScribe(canvas)
-scribe.setDegrees(150)
-scribe.forward(100)
+scribe.plotX(sine)
+scribe.plotX(cosine)
 
 
